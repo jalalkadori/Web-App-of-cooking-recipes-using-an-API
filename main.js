dd09c9e3bc784c1f8e6e9e3c9df6c134d6c89827
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-01-06 20:09:43 +0100

    updating html and js files

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-let searchArea = document.getElementById('searchArea');
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=").then((result) => result.json()).then((data) => {
-    console.log(data.meals[0]);
-    let dataArr = data.meals[0];
-    searchArea.innerHTML = dataArr;
-});
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=").then((result) => result.json()).then((data) => {console.log(data.meals);});
