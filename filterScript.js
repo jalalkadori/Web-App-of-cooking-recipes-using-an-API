aea02f336c299cc30f9faa4b03c70bafe53a5e9e
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-01-15 04:03:52 +0000

    updating script files

--- filterScript.js
+++ filterScript.js
@@ -1,60 +1,38 @@
 const mealsByName = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 var mealsList = [];
-var arrayListHolder = [];
+var arrayListHolder;
 var searchArea = document.getElementById('searchArea');
 fetch(mealsByName)
 .then((result) => result.json()).then((data) => {
     let arrData = data.meals;
     mealsList = arrData;
 });
-
-// function categoryFilter() {
-//     var categoryFilter = document.getElementById("categoryFilter").value;
-//     var filterHolder = [];
-//     var categoryListFiltered = mealsList.filter(item  => item.strCategory === categoryFilter);
-
-//     if (categoryListFiltered != "") {
-//         filterHolder.push(categoryListFiltered);
-//     }
-//     var arrayListHolder = filterHolder[0];
-//     let itemFound = "";
-//     for (let i = 0; i < arrayListHolder.length; i++) {
-//       itemFound += 
-//         `<div class="col-10 col-sm-6 col-md-4 col-lg-3">
-//           <div class="card">
-//             <img src="${arrayListHolder[i].strMealThumb}" alt="image">
-//             <div class="card-body">
-//               <h5 class="card-title">${arrayListHolder[i].strMeal}</h5>
-//               <p class="card-text">${arrayListHolder[i].strArea}</p>
-//               <button type="button" class="btn btn-danger" data-bs-toggle="modal" onclick="displaySelectedMeal(this)"
-//                data-bs-target="#exampleModal">
-//                Show More Infos</button>
-//             </div>
-//           </div>
-//         </div>`
-//     }
-//     searchArea.innerHTML = "";
-//     searchArea.innerHTML = itemFound;
-//     console.log(arrayListHolder)
-// }
+function categoryFilter() {
+    var categoryFilter = document.getElementById("categoryFilter").value;
+    // getting an array with all the elements that matches the selected value
+    var categoryListFiltered = mealsList.filter(item  => item.strCategory === categoryFilter);
+    arrayListHolder = categoryListFiltered;
+    displayOnCards()
+}
 
 function CountryFilter() {
   var areaFilter = document.getElementById("areaFilter").value;
   var areaListFiltered = mealsList.filter(item  => item.strArea === areaFilter);
-
+  arrayListHolder = areaListFiltered;
+  displayOnCards()
+}
+function displayOnCards() {
   searchArea.innerHTML = "";
-  
   let itemFound = "";
-  for (let i = 0; i < areaListFiltered.length; i++) {
-    arrayListHolder = areaListFiltered[i];
+  for (let i = 0; i < arrayListHolder.length; i++) {
     itemFound += 
       `<div class="col-10 col-sm-6 col-md-4 col-lg-3">
         <div class="card">
-          <img src="${areaListFiltered[i].strMealThumb}" alt="image">
+          <img src="${arrayListHolder[i].strMealThumb}" alt="image">
           <div class="card-body">
-            <h5 class="card-title">${areaListFiltered[i].strMeal}</h5>
-            <p class="card-text">${areaListFiltered[i].strArea}</p>
-            <button type="button" class="btn btn-danger" data-bs-toggle="modal" onclick="displaySelectedMeal(this, arrayListHolder)"
+            <h5 class="card-title">${arrayListHolder[i].strMeal}</h5>
+            <p class="card-text">${arrayListHolder[i].strArea}</p>
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" onclick="displaySelectedMeal(this)"
              data-bs-target="#exampleModal">
              Show More Infos</button>
           </div>
@@ -62,15 +40,16 @@ function CountryFilter() {
       </div>`
   }
   searchArea.innerHTML = itemFound;
-  console.log(areaListFiltered)
-  console.log(arrayListHolder)
 }
-function displaySelectedMeal(element, arr) {
+
+
+function displaySelectedMeal(element) {
   var ModalBody = document.getElementById("ModalBody");
   var childrens = searchArea.children;
   var childClicked = element.parentNode.parentNode.parentNode;
   var selectedChildIndex = Array.from(childrens).findIndex(function(child){ return child === childClicked});
-  var selectedChild = arr[selectedChildIndex];
+  var selectedChild = arrayListHolder[selectedChildIndex];
+
   ModalBody.innerHTML = 
     `
     <div class="card">
